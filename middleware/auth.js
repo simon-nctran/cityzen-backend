52265ebc1310d3f0eb5028e4c62047d66fad032a
Simon Tran <sctran@student.unimelb.edu.au>
2020-05-30 19:13:18 +1000

    added additional reference for authentication implementation

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,7 @@
 /* eslint-disable dot-notation */
 const jwt = require("jsonwebtoken");
-// Reference for JWT: https://medium.com/quick-code/handling-authentication-and-authorization-with-node-7f9548fedde8
+// Reference for JWT authentication: https://medium.com/quick-code/handling-authentication-and-authorization-with-node-7f9548fedde8
+// Additional reference: https://medium.com/swlh/jwt-authentication-authorization-in-nodejs-express-mongodb-rest-apis-2019-ad14ec818122
 
 // IMPORTANT: next is a parameter that takes in the next function
 // the .get/post request will handle placing a function in
