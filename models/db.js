989943eb3e93760d5ecd45917a83af3faf104b3b
Simon Tran <sctran@student.unimelb.edu.au>
2020-05-30 18:58:23 +1000

    Refactor callbacks usage into async/await functions; added options to mongoose.connect to remove deprecation warnings

--- models/db.js
+++ models/db.js
@@ -5,12 +5,12 @@ const uri =
   "mongodb+srv://Yutao:wangyutao123@cluster0-diojo.mongodb.net/test?retryWrites=true&w=majority";
 
 /* connect to the database */
-mongoose.connect(uri, (err) => {
-  if (!err) {
-    console.log("Connected to mongo.");
-  } else {
-    console.log("Failed to connect to mongo!", err);
-  }
-});
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  // Options (second parameter) included to remove deprecation warnings
+  // Bonus Note: connect resolves to "this" if connection succeeded (i.e. nothing of value)
+  // https://mongoosejs.com/docs/api.html#mongoose_Mongoose-connect
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error(`Could not connect to MongoDB: ${err.message}`)); // all errors have error.message property
 
 require("./users.js");
