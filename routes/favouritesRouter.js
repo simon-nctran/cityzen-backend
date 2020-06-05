3f4ef25ffddaefa9f30135597127480e97ffce77
Simon Tran <sctran@student.unimelb.edu.au>
2020-06-05 17:27:26 +1000

    apply prettier

--- routes/favouritesRouter.js
+++ routes/favouritesRouter.js
@@ -12,8 +12,11 @@ const authChecker = require("../middleware/auth");
 favouritesRouter.get("/", authChecker, (req, res) =>
   favouritesController.getAllFavourites(req, res)
 );
+
 favouritesRouter.post("/", authChecker, (req, res) => favouritesController.addFavourite(req, res));
-// favouritesRouter.delete("/:id", authChecker, (req, res) => favouritesController.deleteFavourite(req, res) )
-favouritesRouter.delete("/:id", authChecker, (req, res) => favouritesController.deleteFavourite(req, res));
+
+favouritesRouter.delete("/:id", authChecker, (req, res) =>
+  favouritesController.deleteFavourite(req, res)
+);
 
 module.exports = favouritesRouter;
